9b36f31bc00cf3ba372f40517d267bad97db69fd
Florent Poujol <florent.poujol@gmail.com>
2015-09-27 00:11:08 +0200

    Inlcude the docsbrowser plugin.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,19 @@
 var gulp = require("gulp");
-var tasks = [];
+var tasks = [ "jade", "stylus" ];
+
+// Jade
+var jade = require("gulp-jade");
+gulp.task("jade", function() {
+  return gulp.src("./editors/**/index.jade").pipe(jade()).pipe(gulp.dest("./public/editors"));
+});
+
+// Stylus
+var stylus = require("gulp-stylus");
+var nib = require("nib");
+var cssimport = require("gulp-cssimport");
+gulp.task("stylus", function() {
+  return gulp.src("./editors/**/index.styl").pipe(stylus({use: [ nib() ], errors: true})).pipe(cssimport()).pipe(gulp.dest("./public/editors"));
+});
 
 // TypeScript
 var ts = require("gulp-typescript");
@@ -38,12 +52,17 @@ function makeBrowserify(sourcePath, destPath, outputName, standalone, taskName) 
 makeBrowserify("./api/", "./public", "api");
 makeBrowserify("./components/", "./public", "components");
 makeBrowserify("./componentEditors/", "./public", "componentEditors");
+makeBrowserify("./editors/docsbrowser/", "./public/editors", "docsbrowser/index", null, "docsbrowser");
 
 // Watch
 gulp.task("watch", function() {
+  gulp.watch(["./editors/**/*.jade"], ["jade"]);
+  gulp.watch(["./editors/**/*.styl"], ["stylus"]);
+  
   gulp.watch(["./api/*.js", "./api/*.ts"], ["api-browserify"]);
   gulp.watch(["./components/*.js"], ["components-browserify"]);
   gulp.watch(["./componentEditors/*.js"], ["componentEditors-browserify"]);
+  gulp.watch(["./editors/docsbrowser/*.js"], ["docsbrowser-browserify"]);
 
   gulp.watch(["./**/*.ts", "!./api/*.ts"], ["typescript"]);
 });
