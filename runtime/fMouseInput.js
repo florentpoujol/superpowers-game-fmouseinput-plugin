822e119d6abdb0ea3dd77f9fc5fb4efe7155024f
Florent Poujol <florent.poujol@gmail.com>
2015-12-16 18:25:15 +0100

    Update .gitignore.

--- runtime/fMouseInput.js
+++
@@ -1,7 +0,0 @@
-// cofig is of type fMouseInputConfigPub
-// component is the Engine component (fMouseInput)
-function setupComponent(player, component, config) {
-    console.log("mouseinput runtime setupComponent", component, config, player);
-    component.setCameraActorName(config.cameraActorName);
-}
-exports.setupComponent = setupComponent;
