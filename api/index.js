1ed9757ef0d340bf80780c4b876076bd727329a5
Florent Poujol <florent.poujol@gmail.com>
2016-01-01 15:05:58 +0100

    Update for new API plugin registration. Bump version to v0.1.1.

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 
 var fs = require("fs");
 
-SupCore.system.api.registerPlugin("typescript", "fMouseInput", {
+SupCore.system.registerPlugin("typescriptAPI", "fMouseInput", {
   code: fs.readFileSync(__dirname + "/main.ts", { encoding: "utf8" }).replace(/reference path/gi, ""),
   defs: fs.readFileSync(__dirname + "/main.d.ts", { encoding: "utf8" }),
   exposeActorComponent: { propertyName: "fMouseInput", className: "fMouseInput" }
